6a0cdba417bd166a8830f636ff07c06484a14947
kn2798 <taha1hosseini2@yahoo.com>
2024-09-20 00:08:46 +0330

    Tags Modified Removed:   -LEFT_BALL_SPAWNER_NAME   -RIGHT_BALL_SPAWNER_NAME Added:   +HEARTS_TAG   +HEART_TAG   +HEART_IDLE_ANIMATION_TAG   +HEART_REVIVE_ANIMATION_TAG   +HEART_POP_ANIMATION_TAG   +GAMEPLAY_SCENE_TAG   +LEFT_BALL_SPAWNER_NAME   +RIGHT_BALL_SPAWNER_NAME   +HEART_REVIVE_ACTION   +HEART_POP_ACTION   +HEART_REVIVE_ANIMATION_LENGTH

--- Assets/Scripts/Tags.cs
+++ Assets/Scripts/Tags.cs
@@ -13,15 +13,28 @@ public class Tags : MonoBehaviour
 
     public static string BALL_DESTROYER_TAG = "BallDestroyer";
     public static string CANVAS_TAG = "Canvas";
-    
-    public static string LEFT_BALL_SPAWNER_NAME = "BallSpawnerLeftBoundry";
-    public static string RIGHT_BALL_SPAWNER_NAME = "BallSpawnerRightBoundry";
+    public static string HEARTS_TAG = "Hearts";
+    public static string HEART_TAG = "Heart";
+
+    public static string HEART_REVIVE_ANIMATION_TAG = "HeartReviveAnimation";
+    public static string HEART_POP_ANIMATION_TAG = "HeartPopAnimation";
+    public static string HEART_IDLE_ANIMATION_TAG = "HeartIdleAnimation";
+
+    public static string GAMEPLAY_SCENE_TAG = "GameplayScene";
 
     public static string SCORE_UI_PREFIX = "Score: ";
     public static string BEST_SCORE_UI_PREFIX = "Best: ";
 
     public static string BEST_SCORE_PLAYER_PREFS_KEY = "BestScore";
 
+    public static string LEFT_BALL_SPAWNER_NAME = "BallSpawnerLeftBoundry";
+    public static string RIGHT_BALL_SPAWNER_NAME = "BallSpawnerRightBoundry";
+
     public static string SCORE_TEXT_NAME = "ScoreText";
     public static string BEST_SCORE_TEXT_NAME = "BestScoreText";
+
+    public static string HEART_REVIVE_ACTION = "Revive";
+    public static string HEART_POP_ACTION = "Pop";
+
+    public static int HEART_REVIVE_ANIMATION_LENGTH = 500;
 }
