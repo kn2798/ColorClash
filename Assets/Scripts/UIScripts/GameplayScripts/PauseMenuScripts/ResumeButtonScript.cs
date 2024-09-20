b8e6db34e47112812ef60a0a8e9788f4079dddaa
kn2798 <taha1hosseini2@yahoo.com>
2024-09-20 15:48:32 +0330

    Resume Button Script Added
    This Script Resumes the time and Deactivates PauseCanvas

---
+++ Assets/Scripts/UIScripts/GameplayScripts/PauseMenuScripts/ResumeButtonScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResumeButtonScript : MonoBehaviour
+{
+    GameObject PauseCanvas;
+
+    private void Start()
+    {
+        PauseCanvas = GameObject.Find(Tags.PAUSE_CANVAS_NAME);
+    }
+
+    public void ManageResumeButton()
+    {
+        Time.timeScale = 1;
+        PauseCanvas.SetActive(false);
+    }
+}
