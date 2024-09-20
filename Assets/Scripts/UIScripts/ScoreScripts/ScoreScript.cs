55ca20e4eee6fac916ddcab50c978bbe93293beb
kn2798 <taha1hosseini2@yahoo.com>
2024-09-20 15:56:18 +0330

    Score Script Modified
    This Script now Saves Score Variable to PlayerPrefs for Other Classes to use

--- Assets/Scripts/UIScripts/ScoreScripts/ScoreScript.cs
+++ Assets/Scripts/UIScripts/ScoreScripts/ScoreScript.cs
@@ -8,6 +8,7 @@ public class ScoreScript : MonoBehaviour
 {
     public void ManageScore(int Score)
     {
+        PlayerPrefs.SetInt(Tags.SCORE_PLAYER_PREFS_KEY, Score);
         this.gameObject.GetComponent<TextMeshProUGUI>().text = Tags.SCORE_UI_PREFIX + Score;
     }
 }
